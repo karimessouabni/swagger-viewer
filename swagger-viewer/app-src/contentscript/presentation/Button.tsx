1288d68b418b818010cede296d8a3b4efd78ed71
karim <karimessouabni0@gmail.com>
2019-08-12 23:44:39 +0100

    bitbucket adaptation

--- swagger-viewer/app-src/contentscript/presentation/Button.tsx
+++ swagger-viewer/app-src/contentscript/presentation/Button.tsx
@@ -8,7 +8,6 @@ export const Button: React.FC<Props> = ({ onClick, children }) => {
   return (
     <button
       type="button"
-      // 親和性のため、GitHub の button と同じ class を適用する
       className="btn"
       onClick={onClick}
     >
